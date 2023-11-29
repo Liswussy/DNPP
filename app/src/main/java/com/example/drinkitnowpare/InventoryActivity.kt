8b0084746f14f5a37008dd1e13726be56c0a15c3
lewis <lewispunzalan21@gmail.com>
2023-11-30 02:44:42 +0800

    11/24/2023

--- app/src/main/java/com/example/drinkitnowpare/InventoryActivity.kt
+++ app/src/main/java/com/example/drinkitnowpare/InventoryActivity.kt
@@ -1,11 +1,20 @@
 package com.example.drinkitnowpare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
 
-class InventoryActivity : AppCompatActivity() {
+class InventoryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory)
+
+        val btn_stocktake = findViewById<Button>(R.id.btn_stock_take)
+        btn_stocktake.setOnClickListener {
+            val intent = Intent(this, StockTakeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/drinkitnowpare/StockTakeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drinkitnowpare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StockTakeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stock_take)
+    }
+}
